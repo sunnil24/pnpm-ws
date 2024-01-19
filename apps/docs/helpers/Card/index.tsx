b34f7e3b226c0fd0cbab00418e744457ced65286
Arti Grover <arti.grover@publicissapient.com>
2024-01-19 17:19:29 +0530

    v1.0.3

--- apps/docs/helpers/Card/index.tsx
+++ apps/docs/helpers/Card/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState } from 'react';
 
 interface CardProps {
   href: string;
@@ -8,35 +8,37 @@ interface CardProps {
 }
 
 export const Card: FC<CardProps> = ({
-  href,
-  title,
   description,
+  href,
   isExternalLink = false,
+  title,
 }) => {
   const [isHovered, setIsHovered] = useState(false);
   const dynamicProps = isExternalLink
     ? {
-        target: "_blank",
-        rel: "noreferrer",
+        target: '_blank',
+        rel: 'noreferrer',
       }
     : {};
-
+  const a = 23;
+  
   return (
     <a
       href={href}
       {...dynamicProps}
-      className='no-underline hover:underline font-bold text-lg text-white max-w rounded-xl overflow-hidden shadow-lg transform w-100 p-4 cursor-pointer relative h-28 w-64 hover:-translate-y-2 bg-gradient transition-transform'
+      className="no-underline hover:underline font-bold text-lg text-white max-w rounded-xl overflow-hidden shadow-lg transform w-100 p-4 cursor-pointer relative h-28 w-64 hover:-translate-y-2 bg-gradient transition-transform"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       {title}
+      {a}
       {description && (
         <div
           className={`duration-400 absolute bottom-0 ${
-            isHovered ? "opacity-100" : "opacity-0"
+            isHovered ? 'opacity-100' : 'opacity-0'
           }`}
         >
-          <p className='text-white'>{description}</p>
+          <p className="text-white">{description}</p>
         </div>
       )}
     </a>
