4c391c0e5ec6d0ade87a20926900d6334cfb734b
Bhuvan Chawla <bhuvan.chawla@publicissapient.com>
2024-02-12 15:14:30 +0530

    fix(checkbox): checkbox added

---
+++ libs/ui-lib/src/components/atoms/CheckBoxField/CheckBoxField.tsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Checkbox, checkboxPropDefs } from '@radix-ui/themes';
+
+type CheckboxDif = typeof checkboxPropDefs;
+
+export interface CheckboxProps extends CheckboxDif {
+  className?: string;
+}
+export const CheckboxField = (props: CheckboxProps) => {
+  // @ts-expect-error Text Component has some default props which are spreading into the component directly
+  return <Checkbox {...props} />;
+};
