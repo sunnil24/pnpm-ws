43cb40b62206a96c61c70ffddb94d3e2929d4898
kevbarre <kevin.barrett@publicissapient.com>
2024-02-12 17:23:50 +0530

    loader colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
-import { tailwindConfig } from '@xyzproject/tailwind-config';
-import extractDesignSystemVariables from './tailwind.config.viewer';
+import { tailwindConfig } from "@xyzproject/tailwind-config"
+import extractDesignSystemVariables from "./tailwind.config.viewer"
 
-export const presets = [tailwindConfig];
-export const important = true;
+export const presets = [tailwindConfig]
+export const important = true
 export const theme = {
   // ...baseTheme,
   configViewer: {
     themeReplacements: extractDesignSystemVariables(),
   },
-};
+  extend: {
+    colors: {
+      "loader-segment": "#707070",
+      "loader-track": "#C4C4C4",
+    },
+  },
+}
