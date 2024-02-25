3489a2e710c4b8fcd84953620c41c6834c15ff24
Arti Grover <arti.grover@publicissapient.com>
2024-02-25 16:10:38 +0530

    fix: added rules for prettier and jsx

--- packages/react-linting-standards/config/prettier.cjs
+++ packages/react-linting-standards/config/prettier.cjs
@@ -7,6 +7,7 @@ module.exports = {
       singleQuote: true,
       printWidth: 80,
       tabWidth: 2,
+      endOfLine: 'auto',
     },
   ],
 };
--- packages/react-linting-standards/config/react.cjs
+++ packages/react-linting-standards/config/react.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  "react/display-name": "off",
+  'react/display-name': 'off',
   'react/function-component-definition': 'off',
   'react/jsx-filename-extension': [
     'warn',
@@ -10,4 +10,5 @@ module.exports = {
   'react/jsx-props-no-spreading': 'off',
   'react/prop-types': 'off',
   'react/react-in-jsx-scope': 'off',
+  'react/jsx-uses-react': 'off',
 };
