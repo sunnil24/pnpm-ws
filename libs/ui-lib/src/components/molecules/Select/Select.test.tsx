f607ff287052a6e3b0ea332cad088d0b2033586d
Arti Grover <arti.grover@publicissapient.com>
2024-03-04 20:14:58 +0530

    fix: added test cases

--- libs/ui-lib/src/components/molecules/Select/Select.test.tsx
+++ libs/ui-lib/src/components/molecules/Select/Select.test.tsx
@@ -1,21 +1,16 @@
-// // Dropdown.spec.tsx
-// import { render, screen, fireEvent } from "@testing-library/react";
-// import { describe, test, expect } from "vitest";
-// import "@testing-library/jest-dom/vitest";
-// import Select from "./Select";
+// Dropdown.spec.tsx
+import { render, screen } from "@testing-library/react";
+import { describe, test, expect } from "vitest";
+import "@testing-library/jest-dom/vitest";
+import Select from "./Select";
 
-// describe("Select", () => {
-//   const options = ["Apples", "Bananas", "Oranges"];
+describe("Select", () => {
+  const options = ["Apples", "Bananas", "Oranges"];
 
-//   test("renders correctly and displays options", () => {
-//     console.log(render(<Select options={options} />));
+  test("renders correctly and displays options", () => {
+    console.log(render(<Select options={options} />));
 
-//     // Verify that the dropdown trigger button is rendered
-//     const triggerButton = screen.getByText("Apples");
-//     expect(triggerButton).toBeInTheDocument();
-
-//     const optionElement = screen.getByText("Apples");
-//     expect(optionElement).toBeInTheDocument();
-//   });
-
-// });
+    const triggerButton = screen.getByText("New");
+    expect(triggerButton).toBeInTheDocument();
+  });
+});
