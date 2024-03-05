e7912ff571a58dafb92d9943dfdd1f4f4ddb0c68
Arti Grover <arti.grover@publicissapient.com>
2024-03-05 17:56:20 +0530

    fix: added label for

--- libs/ui-lib/src/components/atoms/Label/Label.tsx
+++ libs/ui-lib/src/components/atoms/Label/Label.tsx
@@ -3,15 +3,16 @@
 export interface LabelProps {
   labelText: string;
   className?: string;
+  labelFor?: string;
 }
 
-const Label: React.FC<LabelProps> = ({ labelText, className }) => {
+const Label: React.FC<LabelProps> = ({ labelText, className, labelFor }) => {
   // Component logic here
 
   return (
     <label
       className={`font-semibold text-sm text-basics-grey70 pb-2 ${className}`}
-      htmlFor="name"
+      htmlFor={labelFor}
     >
       {labelText}
     </label>
