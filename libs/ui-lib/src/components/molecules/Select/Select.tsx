bd5d13391117699feaf11f7b6998c4b03fff6ae8
Arti Grover <arti.grover@publicissapient.com>
2024-03-05 15:39:47 +0530

    fix: added css

--- libs/ui-lib/src/components/molecules/Select/Select.tsx
+++ libs/ui-lib/src/components/molecules/Select/Select.tsx
@@ -16,7 +16,7 @@ const SelectItem = React.forwardRef<HTMLDivElement, SelectItemProps>(
   ({ children, ...props }, forwardedRef) => {
     return (
       <RadixSelect.Item
-        className="text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1 hover:bg-basics-grey10"
+        className="text-[13px] focus:outline-none leading-none text-violet11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1 hover:bg-basics-grey10"
         {...props}
         ref={forwardedRef}
       >
@@ -33,14 +33,36 @@ SelectItem.displayName = "SelectItem";
 
 interface SelectProps {
   options: string[];
+  isDisabled: boolean;
+  defaultValue: string;
+  handleSelectClick?: () => void;
 }
 
-const Select: React.FC<SelectProps> = ({ options }) => {
+const Select: React.FC<SelectProps> = ({
+  options,
+  handleSelectClick,
+  isDisabled = false,
+}) => {
+  let color = "bg-white";
+  if (isDisabled) {
+    color = "bg-basics-grey10";
+  }
   return (
     <RadixSelect.Root>
-      <RadixSelect.Trigger className="inline-flex items-center justify-between rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-white text-violet11 shadow-[0_2px_10px] shadow-black/10 hover:bg-mauve3 focus:shadow-[0_0_0_2px] data-[placeholder]:text-violet9 outline-none w-32">
+      <RadixSelect.Trigger
+        className={`${color} inline-flex items-center disabled justify-between rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px]  text-violet11 hover:bg-mauve3 data-[placeholder]:text-violet9 outline-none w-48`}
+        onPointerDown={(e) => {
+          if (isDisabled) {
+            e.preventDefault();
+          } else {
+            if (handleSelectClick) {
+              handleSelectClick();
+            }
+          }
+        }}
+      >
         <RadixSelect.Value placeholder="New" />
-        <RadixSelect.Icon className="text-violet11">
+        <RadixSelect.Icon className="text-violet11 text-gray-500">
           <ChevronDownIcon className="w-4 h-6" />
         </RadixSelect.Icon>
       </RadixSelect.Trigger>
@@ -49,7 +71,7 @@ const Select: React.FC<SelectProps> = ({ options }) => {
           position="popper"
           side="bottom"
           align="center"
-          className="overflow-hidden bg-white rounded-md shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]"
+          className="overflow-hidden min-w-[12rem] bg-white rounded-md shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]"
         >
           <RadixSelect.ScrollUpButton className="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default">
             <ChevronUpIcon />
