278b462226b41b70ad6670c5e63ee465da2599a9
Jaikishan Verma <jaiverma1@WKMZT7A658B0.global.publicisgroupe.net>
2024-12-19 21:15:26 +0530

    Import Design system variable

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 import { baseTheme, tailwindConfig } from "@sldl/tailwind-config";
-// import extractDesignSystemVariables from "./tailwind.config.viewer";
+import extractDesignSystemVariables from "./tailwind.config.viewer";
 
 export const presets = [tailwindConfig];
 export const important = true;
 export const theme = {
   ...baseTheme,
-  // configViewer: {
-  //   themeReplacements: extractDesignSystemVariables(),
-  // },
+  configViewer: {
+    themeReplacements: extractDesignSystemVariables(),
+  },
 };
